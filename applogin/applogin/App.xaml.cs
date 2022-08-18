5e4f6636793bf8fef58ced0cb61d27c09a6607c2
aspiregeovani <aspiregeovani@ASPIREGEOVANI-N>
2022-08-18 20:39:33 -0300

    Adicionar arquivos de projeto.

---
+++ applogin/applogin/App.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarim.Forms.Xaml;
+using System.Collections.Generic;
+
+
+using applogin.View;
+using applogin.Model;
+
+namespace applogin
+{
+    public partial class App : Application
+    {
+        public List<DadosUsuario> list_usuarios = new List<DadosUsuario>
+        {
+            new DadosUsuario()
+            {
+                Email = "aspiregabriela@gmail.com",
+                Nome  = "Aluno",
+                Senha = "123"
+            },
+
+            new DadosUsuario()
+            {
+                Email = "aspiregeovane@gmail.com",
+                Nome  = "Coordenador",
+                Senha = "456"
+            }
+        };
+
+        public App ()
+        {
+            InitializeComponent();
+            if (Properties.ContainsKey("usuario_logado"))
+                MainPage = new Protegida();
+            else
+                MainPage = new login();
+        }
+
+        protected override void OnStart()
+        {
+            
+        }
+
+        protected override void OnSleep()
+        {
+
+        }
+
+        protected override void OnResume()
+        {
+
+        }
+    }
+
+}
---
+++ applogin/applogin/Model/DadosUsuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace applogin.Model
+{
+    public class DadosUsuario : ContentPage
+    {
+        public DadosUsuario()
+        { }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nome { get; set; }
+    
+    }
+}
